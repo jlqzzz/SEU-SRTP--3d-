77df31fe16b97a64ce180ac7472bb1619f81d337
chonepieceyb <34810016+chonepieceyb@users.noreply.github.com>
2019-03-12 21:54:37 +0800

    初步验证了关于投影的想法

--- prepare_data/kitti_object.py
+++ prepare_data/kitti_object.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pickle as pickle
+import numpy as np 
 
 PREPARE_DIR=os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR=os.path.dirname(PREPARE_DIR)
@@ -9,3 +10,29 @@ KITTI_DIR=os.path.join(ROOT_DIR,'dataset\\kitti')
 def class kitti_object(object):
     def __init__( self , keyword='training', dir=KITTI_DIR ):
 '''
+class Object3d(object): #3D物体类标签定义 -H
+    ''' 3d object label '''
+    def __init__(self, label_file_line):
+        data = label_file_line.split(' ')  #以空格分隔 -H  ps:返回分割后的所有的子字符串，每一个label_file_line就是一个目标 eg： Pedestrian 0.00 0 -0.20 712.40 143.00 810.73 307.92 1.89 0.48 1.20 1.84 1.47 8.41 0.01 -Y
+
+        data[1:] = [float(x) for x in data[1:]]
+
+        # extract label, truncation, occlusion //提取标签，截断，遮挡 —H
+        self.type = data[0] # 'Car', 'Pedestrian', ...//物体种类
+        self.truncation = data[1] # truncated pixel ratio [0..1]  截断程度 h
+        self.occlusion = int(data[2]) # 0=visible, 1=partly occluded, 2=fully occluded, 3=unknown 被遮挡程度 h
+        self.alpha = data[3] # object observation angle [-pi..pi] 观察角度 h
+
+        # extract 2d bounding box in 0-based coordinates
+        self.xmin = data[4] # left
+        self.ymin = data[5] # top
+        self.xmax = data[6] # right
+        self.ymax = data[7] # bottom
+        self.box2d = np.array([self.xmin,self.ymin,self.xmax,self.ymax])
+        
+        # extract 3d bounding box information
+        self.h = data[8] # box height
+        self.w = data[9] # box width
+        self.l = data[10] # box length (in meters)
+        self.t = (data[11],data[12],data[13]) # location (x,y,z) in camera coord.
+        self.ry = data[14] # rotation_y yaw angle (around Y-axis in camera coordinates) [-pi..pi]  
--- prepare_data/prepare_data.py
+++ prepare_data/prepare_data.py
@@ -9,12 +9,16 @@ from mayavi import mlab
 PREPARE_DIR=os.path.dirname(os.path.abspath(__file__))
 #项目文件的根目录
 ROOT_DIR=os.path.dirname(PREPARE_DIR)
-#照片文件的根目录
-PICTURE_DIR=os.path.join(kitti_object.KITTI_DIR,'object\\training\image_2')
+#照片文件2的根目录
+PICTURE2_DIR=os.path.join(kitti_object.KITTI_DIR,'object\\training\image_2')
+#照片文件3的根目录
+PICTURE3_DIR=os.path.join(kitti_object.KITTI_DIR,'object\\training\image_3')
 #雷达文件的根目录
 LIDAR_DIR=os.path.join(kitti_object.KITTI_DIR,'object\\training\\velodyne')
 #标定文件的根目录
 CALIB_DIR=os.path.join(kitti_object.KITTI_DIR,'object\\training\calib')
+#LABLE文件的根目录
+LABEL_DIR=os.path.join(kitti_object.KITTI_DIR,'object\\training\label_2')
 print(LIDAR_DIR)
 
 
@@ -22,8 +26,8 @@ print(LIDAR_DIR)
 #测试内容
 #尝试画出点云数据
 #尝试画出图片
-print(PICTURE_DIR)
-filename_dir=os.path.join(PICTURE_DIR,'000000.png')
+print(PICTURE2_DIR)
+filename_dir=os.path.join(PICTURE2_DIR,'000000.png')
 print(filename_dir)
 picture=cv2.imread(filename_dir)
 cv2.namedWindow('picture',cv2.WINDOW_AUTOSIZE)
@@ -42,21 +46,28 @@ mlab.points3d(scan[:,0], scan[:,1], scan[:,2], scan[:,1], mode='point',    color
 mlab.show()
 '''
 
+#读取点云数据
+Lidar_file_dir=os.path.join(LIDAR_DIR,'000001.bin')
+scan = np.fromfile(Lidar_file_dir, dtype=np.float32)
+scan = scan.reshape((-1, 4))
+
 #尝试读取标定信息
-'''
-calib_file_dir=os.path.join(CALIB_DIR,'000000.txt')
+
+calib_file_dir=os.path.join(CALIB_DIR,'000001.txt')
 calib_data={}
 with open(calib_file_dir,'r') as rf:
     for line in rf.readlines():
-        if len(line)==0:
+        if len(line)<=1:
             continue
         line=line.rstrip(" ")
         key,value=line.split(':',1)
         calib_data[key]=np.array([float(x) for x in value.split()])
  
 P2=calib_data['P2']
-
 P2=np.reshape(P2,[3,4])
+
+P3=calib_data['P3']
+P3=np.reshape(P3,[3,4])
 
 R0_rect=calib_data['R0_rect']
 R0_rect=np.reshape(R0_rect,[3,3])
@@ -64,9 +75,86 @@ R0_rect=np.reshape(R0_rect,[3,3])
 Tr_velo_to_cam=calib_data['Tr_velo_to_cam']
 Tr_velo_to_cam=np.reshape(Tr_velo_to_cam,[3,4])
 
-print(Tr_velo_to_cam)
-'''
 
+#尝试读取label文件
+label_file_dir=os.path.join(LABEL_DIR,'000001.txt')
+object_3d=[]
+with open(label_file_dir,'r') as rf:
+    for line in rf.readlines():
+        if len(line)==0:
+            continue
+        object_3d.append(kitti_object.Object3d(line))
 #尝试写投影的函数
+#代码从frustum-pointnets中复制来的
 
 
+def project_velo_to_ref(pts_3d_velo):                #把雷达坐标系下的点 投影到参考相机坐标系（ref坐标系）中,输入是 3*4
+    return np.dot(pts_3d_velo, np.transpose(Tr_velo_to_cam))#self.V2C (3,4)  ,输出是 n*3
+
+def project_ref_to_rect(pts_3d_ref):
+   #  Input and Output are nx3 points 
+    return np.transpose(np.dot(R0_rect, np.transpose(pts_3d_ref)))
+
+
+def project_rect_to_image( pts_3d_rect,PX):
+   #    Input: nx3 points in rect camera coord.
+    #    Output: nx2 points in image2 coord.
+    #
+    n = pts_3d_rect.shape[0]
+    pts_3d_rect=np.hstack((pts_3d_rect , np.ones((n,1))))   #增加一个维度
+    pts_2d = np.dot(pts_3d_rect, np.transpose(PX)) # nx3
+    pts_2d[:,0] /= pts_2d[:,2]
+    pts_2d[:,1] /= pts_2d[:,2]
+    return pts_2d[:,0:2]
+
+#过滤掉不在2D框中的点云数据
+def extract_pts_from_2D(pts_2D,xmin,ymin,xmax,ymax):   #pts_2D是 n*2的,返回掩模
+    fov_inds =(pts_2D[:,0] >= xmin) & (pts_2D[:,0]<=xmax )& ( pts_2D[:,1]>=ymin ) & ( pts_2D[:,1]<=ymax)
+    return fov_inds
+
+#把点云投影到ref坐标系上
+pts_in_ref=project_velo_to_ref(scan)
+
+#把点云投影到rect坐标系上
+pts_in_rect=project_ref_to_rect(pts_in_ref)
+
+#把点云投影到 iamge2坐标系上
+
+pts_in_image_2D=project_rect_to_image(pts_in_rect,P2)
+
+
+fov_2d_inds=extract_pts_from_2D(pts_in_image_2D, object_3d[0].xmin, object_3d[0].ymin, object_3d[0].xmax, object_3d[0].ymax)
+
+pts_fov_in_rect=pts_in_rect[fov_2d_inds,:]
+
+#把过滤出来的点云投影到第二张照片上
+pts_fov_in_image3=project_rect_to_image(pts_fov_in_rect,P3)
+#找到这些点云的边界
+print(fov_2d_inds)
+
+xmin3=pts_fov_in_image3[0,0] ;ymin3=pts_fov_in_image3[0,1];xmax3=pts_fov_in_image3[0,0];ymax3=pts_fov_in_image3[0,1]
+for p in pts_fov_in_image3:
+    if p[0]<xmin3:
+        xmin3=p[0]
+    if p[0]>xmax3:
+        xmax3=p[0]
+    if p[1]<ymin3:
+        ymin3=p[1]
+    if p[1]>ymax3:
+        ymax3=p[1]
+print(xmin3,ymin3,xmax3,ymax3)    
+
+filename3_dir=os.path.join(PICTURE3_DIR,'000001.png')
+filename2_dir=os.path.join(PICTURE2_DIR,'000001.png')
+picture2=cv2.imread(filename2_dir)
+picture3=cv2.imread(filename3_dir)
+cv2.namedWindow('picture2',cv2.WINDOW_AUTOSIZE)
+cv2.namedWindow('picture3',cv2.WINDOW_AUTOSIZE)
+cv2.rectangle(picture2, ( int(object_3d[0].xmin),int(object_3d[0].ymin) ),( int(object_3d[0].xmax) ,int(object_3d[0].ymax)) ,(0,255,0))
+cv2.rectangle(picture3,(int(xmin3),int(ymin3)),(int(xmax3),int(ymax3)),(0,255,0))
+cv2.imshow('picture2',picture2)
+cv2.imshow('picture3',picture3)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+            
